c0d8ae382bba9c249c05a2e3eb9b64158ea0f609
sushmagnt9 <sushmagnt9@gmail.com>
2022-08-26 16:31:32 +0530

    routepath

--- src/app/service/app-routing.module.ts
+++ src/app/service/app-routing.module.ts
@@ -4,6 +4,7 @@ import { BooksComponent } from '../books/books.component';
 import { CreatebooksComponent } from '../createbooks/createbooks.component';
 import { GetpaymentComponent } from '../getpayment/getpayment.component';
 import { PaymentComponent } from '../payment/payment.component';
+import { SearchbookComponent } from '../searchbook/searchbook.component';
 import { SigninComponent } from '../signin/signin.component';
 import { SignupComponent } from '../signup/signup.component';
 
@@ -12,7 +13,8 @@ const routes: Routes = [{path : 'Signin' , component : SigninComponent},
 {path : 'Signup' , component : SignupComponent },
 {path : 'Payment', component : PaymentComponent},
 {path : 'createbooks', component : CreatebooksComponent},
-{path : 'getpayment', component : GetpaymentComponent}
+{path : 'getpayment', component : GetpaymentComponent},
+{path : 'searchbooks', component : SearchbookComponent}
 ];
 
 @NgModule({
