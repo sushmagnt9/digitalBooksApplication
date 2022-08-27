fe8b2623e29e8b8b9a74648e283f67b3a749b828
sushmagnt9 <sushmagnt9@gmail.com>
2022-08-27 13:38:47 +0530

    payment redirect

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
 import { Payment } from '../models/payment';
 import { paymentService } from '../service/payment.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -17,17 +19,19 @@ export class PaymentComponent implements OnInit {
     bookId:0,
     paymentDate:new Date()
   }
-  constructor(private paymentService : paymentService) { }
+  constructor(private paymentService : paymentService,private router : Router) { }
 
   ngOnInit(): void {
   }
+  response:any;
   onSubmit(): void {
-    if(this.payment.buyerEmail!=''  ){
+    if(this.payment.buyerEmail!='' ){
         this.paymentService.CreatePayment(this.payment)
         .subscribe(
           response => {
+            this.response = response;
             console.log(response);
-            
+            this.router.navigate(['/getpayment']);
           }
         )
       }else{
